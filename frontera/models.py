d6e9780cd799ffeb98cd736d0d85260ac4392591
danila_rostovtsev <rostovtsev.danila@gmail.com>
2022-04-09 14:22:29 +0400

    Init with User model and auth

---
+++ frontera/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    email = models.EmailField(verbose_name="email", unique=True)
+
+    class Meta:
+        verbose_name = "user"
