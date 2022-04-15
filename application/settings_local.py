c597ab781de38e7509221b0ef31717bf68e105d4
danila_rostovtsev <rostovtsev.danila@gmail.com>
2022-04-15 15:53:26 +0400

    Add postgres settings

---
+++ application/settings_local.py
@@ -0,0 +1,14 @@
+from settings import *
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5444',
+    }
+}
